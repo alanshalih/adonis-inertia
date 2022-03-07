735d585fd59e7d2a413938ce3760416d04f93a72
unknown <maulanashalihin@gmail.com>
2022-03-07 14:12:37 +0800

    adonis inertia

---
+++ resources/js/Pages/Test.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let nama = "Danang";
+  import {   Link } from '@inertiajs/inertia-svelte'
+</script>
+
+<h1>Halo {nama}</h1>
+<Link href="/">Home</Link>
+<Link href="/test">Test</Link>
+
+ 
